edcf1ec40d821803620317a9f1b0f36118c788a0
castro <xcaze8@gmail.com>
2024-02-18 00:58:56 +0100

    About page

--- src/content/blog/markdown-style-guide.md
+++ src/content/blog/markdown-style-guide.md
@@ -3,7 +3,7 @@ title: 'Markdown Style Guide'
 description: 'A guide to the ultimate server'
 date: 2022-07-08
 image: '/blog-placeholder-3.jpg'
-tags: ["blog", "astro"]
+tags: ["blog", "anything"]
 ---
 
 Here is a sample of some basic Markdown syntax that can be used when writing Markdown content in Astro.
--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,7 @@ import { z, defineCollection } from 'astro:content';
 const blogCollection = defineCollection({
   type: 'content', // v2.5.0 and later
   schema: z.object({
-  title: z.string(),
+    title: z.string(),
 	description: z.string().max(160, {message: "Description should be less than 160 characters."}),
 	date: z.date(),
 	image: z.string().optional(),
